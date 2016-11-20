98e6569d79e4fd4d18a3af978c8c163c2848f5d9
VratislavHais <HaisVratislav@seznam.cz>
2016-11-20 14:55:33 +0100

    triggers and procedures

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+/* Droping previous data*/
 DROP TABLE expozice_umelce;
 DROP TABLE mistnosti_objednavky;
 DROP TABLE vybaveni_mistnosti;
@@ -10,6 +11,17 @@ DROP TABLE expozice;
 DROP TABLE umelec;
 DROP TABLE zamestnanec;
 
+DROP SEQUENCE seq_expozice;
+DROP SEQUENCE seq_umelec;
+DROP SEQUENCE seq_zamestnanec;
+DROP SEQUENCE seq_mistnost;
+DROP SEQUENCE seq_vybaveni_mistnosti;
+DROP SEQUENCE seq_pronajimatel;
+DROP SEQUENCE seq_objednavka;
+
+
+
+/* Creating new tables*/
 CREATE TABLE expozice(
 	id_expozice int NOT NULL,
 	typ VARCHAR(40) NOT NULL,
@@ -31,12 +43,11 @@ CREATE TABLE zamestnanec(
 	id_zamestnance int NOT NULL,
 	jmeno VARCHAR(40),
 	prijmeni VARCHAR(40),
-	heslo VARCHAR(20) NOT NULL,
+  heslo VARCHAR(20) NOT NULL,
 	datum_nar DATE NOT NULL,
 	prava int NOT NULL,
 	rod_cislo number(10) NOT NULL,
-	plat int NOT NULL,
-	CHECK(round(rod_cislo/11.0) = rod_cislo/11.0)
+	plat int NOT NULL
 );
 
 CREATE TABLE mistnost(
@@ -64,8 +75,7 @@ CREATE TABLE pronajimatel(
 
 CREATE TABLE fyz_osoba(
 	id_pronajimatele int NOT NULL,
-	rod_cislo number(10) NOT NULL,
-	CHECK(round(rod_cislo/11.0) = rod_cislo/11.0)
+	rod_cislo number(10) NOT NULL
 );
 
 CREATE TABLE prav_osoba(
@@ -93,6 +103,7 @@ CREATE TABLE expozice_umelce(
 	id_expozice int NOT NULL
 );
 
+/* Adding primary and foreign keys to tables*/
 ALTER TABLE expozice ADD PRIMARY KEY (id_expozice);
 ALTER TABLE umelec ADD PRIMARY KEY (id_umelce);
 ALTER TABLE zamestnanec ADD PRIMARY KEY (id_zamestnance);
@@ -127,45 +138,209 @@ ALTER TABLE fyz_osoba ADD FOREIGN KEY (id_pronajimatele) REFERENCES pronajimatel
 ALTER TABLE prav_osoba ADD PRIMARY KEY (ICO);
 ALTER TABLE prav_osoba ADD FOREIGN KEY (id_pronajimatele) REFERENCES pronajimatel(id_pronajimatele) ON DELETE CASCADE;
 
-INSERT INTO zamestnanec (id_zamestnance, jmeno, prijmeni, datum_nar, prava, rod_cislo, plat) VALUES('001', 'Jan', 'Dvo≈ô√°k', TO_DATE('1954-03-12','YYYY-MM-DD'), '1', '5412036069', '35650');
-INSERT INTO zamestnanec (id_zamestnance, jmeno, prijmeni, datum_nar, prava, rod_cislo, plat) VALUES('002', 'Aneta', 'Ma≈°liƒçkov√°', TO_DATE('1989-06-24','YYYY-MM-DD'), '3', '8956248763', '15650');
-INSERT INTO zamestnanec (id_zamestnance, jmeno, prijmeni, datum_nar, prava, rod_cislo, plat) VALUES('003', 'Ludmila', '≈†√≠pkov√°', TO_DATE('1956-07-26','YYYY-MM-DD'), '2', '5657264459', '17650');
+/* Creating sequences for IDs*/
+CREATE SEQUENCE seq_expozice MINVALUE 0 NOMAXVALUE START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_umelec MINVALUE 0 NOMAXVALUE START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_zamestnanec MINVALUE 0 NOMAXVALUE START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_mistnost MINVALUE 0 NOMAXVALUE START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_vybaveni_mistnosti MINVALUE 0 NOMAXVALUE START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_pronajimatel MINVALUE 0 NOMAXVALUE START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE seq_objednavka MINVALUE 0 NOMAXVALUE START WITH 1 INCREMENT BY 1;
 
-INSERT INTO pronajimatel (id_pronajimatele, nazev, kontakt, poplatek) VALUES('001', 'UMPRUM', '731548762', 'ano');
-INSERT INTO pronajimatel (id_pronajimatele, nazev, kontakt, poplatek) VALUES('002', 'Pavel Nov√°k', '609548795', 'ne');
-INSERT INTO pronajimatel (id_pronajimatele, nazev, kontakt, poplatek) VALUES('003', 'Antotn√≠n kratochv√≠l', '581548221', 'ano');
+/* Creating triggers for automatic id sequencing*/
+CREATE OR REPLACE TRIGGER tr_expozice
+  BEFORE INSERT ON expozice
+  FOR EACH ROW
+  BEGIN
+    SELECT seq_expozice.NEXTVAL INTO :NEW.id_expozice FROM DUAL;
+  END;
+  /
+  
+  CREATE OR REPLACE TRIGGER tr_umelec
+  BEFORE INSERT ON umelec
+  FOR EACH ROW
+  BEGIN
+    SELECT seq_umelec.NEXTVAL INTO :NEW.id_umelce FROM DUAL;
+  END;
+  /
+  
+  CREATE OR REPLACE TRIGGER tr_zamestnanec
+  BEFORE INSERT ON zamestnanec
+  FOR EACH ROW
+  BEGIN
+    SELECT seq_zamestnanec.NEXTVAL INTO :NEW.id_zamestnance FROM DUAL;
+  END;
+  /
+  
+  CREATE OR REPLACE TRIGGER tr_mistnost
+  BEFORE INSERT ON mistnost
+  FOR EACH ROW
+  BEGIN
+    SELECT seq_mistnost.NEXTVAL INTO :NEW.id_mistnosti FROM DUAL;
+  END;
+  /
+  
+  CREATE OR REPLACE TRIGGER tr_vybaveni_mistnosti
+  BEFORE INSERT ON vybaveni_mistnosti
+  FOR EACH ROW
+  BEGIN
+    SELECT seq_vybaveni_mistnosti.NEXTVAL INTO :NEW.id_vybaveni FROM DUAL;
+  END;
+  /
+  
+  CREATE OR REPLACE TRIGGER tr_pronajimatel
+  BEFORE INSERT ON pronajimatel
+  FOR EACH ROW
+  BEGIN
+    SELECT seq_pronajimatel.NEXTVAL INTO :NEW.id_pronajimatele FROM DUAL;
+  END;
+  /
+  
+  CREATE OR REPLACE TRIGGER tr_objednavka
+  BEFORE INSERT ON objednavka
+  FOR EACH ROW
+  BEGIN
+    SELECT seq_objednavka.NEXTVAL INTO :NEW.id_objednavky FROM DUAL;
+  END;
+  /
+  
+  /* Creating trigger to check correctness of ID number of person*/
+  CREATE OR REPLACE TRIGGER tr_rodne_c_z
+  AFTER UPDATE OR INSERT ON zamestnanec
+  FOR EACH ROW
+  DECLARE
+    cislo zamestnanec.rod_cislo%TYPE;
+    rok NUMBER;
+    mesic NUMBER;
+    den NUMBER;
+    koncovka NUMBER;
+  BEGIN
+    cislo := :NEW.rod_cislo;
+    rok := SUBSTR(cislo,1,2);
+    mesic := SUBSTR(cislo,3,2);
+    den := SUBSTR(cislo,5,2);
+    koncovka := SUBSTR(cislo,7,4);
+    IF (LENGTH(cislo) != 10) THEN 
+      Raise_Application_Error (-20220, 'Nespravne pocet cislic rodneho cisla!');
+    END IF;
+    IF (mesic > 50) THEN
+      mesic := mesic - 50;
+    END IF;
+    IF ((mesic < '01') OR (mesic > '12') OR (den < '01') OR (den > '31') OR (koncovka = '0000')) THEN
+      Raise_Application_Error (-20221, 'Nespravne zadane rodne cislo!');
+    END IF;
+    IF (MOD(TO_NUMBER(cislo), 11) != 0) THEN 
+      Raise_Application_Error (-20222, 'Rodne cislo nedelitelne 11!');
+    END IF;
+  END;
+  /
+
+/* Creating procedures for easier work with data */
+
+/* Pocedure that calculate needed time to repaint the room */
+SET serveroutput ON;
+CREATE OR REPLACE PROCEDURE doba_malovani(id_mist IN NUMBER)
+IS
+  CURSOR mistnosti IS SELECT * FROM mistnost;
+  uk_mistnost mistnost%ROWTYPE;
+  cas REAL;
+BEGIN
+  cas := 0;
+  OPEN mistnosti;
+  LOOP
+    FETCH mistnosti INTO uk_mistnost;
+    EXIT WHEN mistnosti%NOTFOUND;
+    IF (uk_mistnost.id_mistnosti = id_mist) THEN
+      cas := uk_mistnost.plocha * 0.5;
+    END IF;
+  END LOOP;
+  CLOSE mistnosti;
+  dbms_output.put_line('Mistnost se bude malovat ' || cas || ' hodin.');
+EXCEPTION
+  WHEN OTHERS THEN
+    Raise_Application_Error (-20201, 'Chyba!');
+END;
+/
+
+SET serveroutput ON;
+CREATE OR REPLACE PROCEDURE kolik_vybaveni(id_mist IN NUMBER, typ_vybav IN VARCHAR)
+IS
+  CURSOR vybav IS SELECT * FROM vybaveni_mistnosti;
+  uk_vybav vybaveni_mistnosti%ROWTYPE;
+  vsechny NUMBER;
+  zvolene NUMBER;
+BEGIN
+  vsechny :=0;
+  zvolene :=0;
+  OPEN vybav;
+  LOOP
+    FETCH vybav INTO uk_vybav;
+    EXIT WHEN vybav%NOTFOUND;
+    IF (id_mist = uk_vybav.id_mistnosti) THEN
+      vsechny := vsechny + uk_vybav.pocet;
+      IF (typ_vybav = uk_vybav.typ) THEN
+        zvolene := zvolene + uk_vybav.pocet;
+      END IF;
+    END IF;
+  END LOOP;
+  dbms_output.put_line('Zarizeni ' || typ_vybav || ' se v mistnosti nachazi ' || zvolene || ' co predstavuje ' || (zvolene * 100)/vsechny || '% .');
+EXCEPTION
+  WHEN ZERO_DIVIDE THEN
+    dbms_output.put_line('Zadane zarizeni se v mistnosti nenachazi.');
+  WHEN OTHERS THEN
+    Raise_Application_Error (-20201, 'Chyba!');
+END;
+/
+
+
+/* Inserting data to tables*/
+INSERT INTO zamestnanec (id_zamestnance, jmeno, prijmeni, datum_nar, prava, rod_cislo, plat) VALUES(null, 'Jan', 'Dvo¯·k', TO_DATE('1954-03-12','YYYY-MM-DD'), '1', '5412036069', '35650');
+INSERT INTO zamestnanec (id_zamestnance, jmeno, prijmeni, datum_nar, prava, rod_cislo, plat) VALUES(null, 'Aneta', 'MaöliËkov·', TO_DATE('1989-06-24','YYYY-MM-DD'), '3', '8956248763', '15650');
+INSERT INTO zamestnanec (id_zamestnance, jmeno, prijmeni, datum_nar, prava, rod_cislo, plat) VALUES(null, 'Ludmila', 'äÌpkov·', TO_DATE('1956-07-26','YYYY-MM-DD'), '2', '5657264459', '17650');
+
+INSERT INTO pronajimatel (id_pronajimatele, nazev, kontakt, poplatek) VALUES(null, 'UMPRUM', '731548762', 'ano');
+INSERT INTO pronajimatel (id_pronajimatele, nazev, kontakt, poplatek) VALUES(null, 'Pavel Nov·k', '609548795', 'ne');
+INSERT INTO pronajimatel (id_pronajimatele, nazev, kontakt, poplatek) VALUES(null, 'AntotnÌn kratochvÌl', '581548221', 'ano');
 
 INSERT INTO fyz_osoba(id_pronajimatele, rod_cislo) VALUES('2', '5657264470');
 INSERT INTO prav_osoba(id_pronajimatele, ICO) VALUES('1', '12345678');
 INSERT INTO prav_osoba(id_pronajimatele, ICO) VALUES('1', '87654321');
 
-INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES('001', 'sochy', '59', '1200', 'ƒçtverec', '001');
-INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES('002', 'sochy', '20', '1500', 'kruh', '001');
-INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES('003', 'instalace', '160', '1200', 'ƒçtverec', '001');
-INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES('004', 'instalace', '70', '1200', 'ƒçtverec', '003');
+INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES(null, 'sochy', '59', '1200', 'Ëtverec', '001');
+INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES(null, 'sochy', '20', '1500', 'kruh', '001');
+INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES(null, 'instalace', '160', '1200', 'Ëtverec', '001');
+INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES(null, 'instalace', '70', '1200', 'Ëtverec', '003');
 
-INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES('001', 'podstavec hlin√≠kov√Ω', '30', '002');
-INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES('002', 'podstavec kamen√Ω', '30', '004');
-INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES('003', 'r√°m d≈ôevƒõn√Ω', '50', '001');
-INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES('004', 'r√°m hlin√≠kov√Ω', '100', '002');
-INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES('005', 'z√°≈ôivka b√≠l√°', '60', '001');
-INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES('006', 'z√°≈ôivka barevn√°', '20', '003');
-INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES('007', 'stojan d≈ôevƒõn√Ω', '15', '003');
-INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES('008', 'r√°m popisovac√≠', '200', '004');
-INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES('009', 'st≈Øl rozkl√°dac√≠', '6', '001');
-INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES('010', '≈æidle d≈ôevƒõn√°', '50', '004');
+-- adding more data for index speed testing
+INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES(null, 'obrazy', '90', '1800', 'obdelnik', '001');
+INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES(null, 'instalace', '45', '1400', 'Ëtverec', '003');
+INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES(null, 'sochy', '38', '1100', 'Ëtverec', '003');
+INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES(null, 'instalace', '70', '1200', 'kruh', '001');
+INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES(null, 'obrazy', '95', '1900', 'obdelnik', '002');
+INSERT INTO mistnost (id_mistnosti, typ_exp, plocha, cena, tvar, id_zamestnance) VALUES(null, 'sochy', '32', '1100', 'Ëtverec', '002');
 
-INSERT INTO umelec (id_umelce, jmeno, prijmeni, specializace, id_zamestnance) VALUES('001', 'Pavel', 'Nov√°k', 'socha≈ôstv√≠', '001');
-INSERT INTO umelec (id_umelce, jmeno, prijmeni, specializace, id_zamestnance) VALUES('002', 'Antotn√≠n', 'Kratochv√≠l', 'fotografie', '003');
-INSERT INTO umelec (id_umelce, jmeno, prijmeni, specializace, id_zamestnance) VALUES('003', 'Fratni≈°ek Ringo', 'ƒåech', 'malba', '003');
+INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES(null, 'podstavec hlinÌkov˝', '30', '002');
+INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES(null, 'podstavec kamen˝', '30', '004');
+INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES(null, 'r·m d¯evÏn˝', '50', '001');
+INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES(null, 'r·m hlinÌkov˝', '100', '002');
+INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES(null, 'z·¯ivka bÌl·', '60', '001');
+INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES(null, 'z·¯ivka barevn·', '20', '003');
+INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES(null, 'stojan d¯evÏn˝', '15', '003');
+INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES(null, 'r·m popisovacÌ', '200', '004');
+INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES(null, 'st˘l rozkl·dacÌ', '6', '001');
+INSERT INTO vybaveni_mistnosti (id_vybaveni, typ, pocet, id_mistnosti) VALUES(null, 'ûidle d¯evÏn·', '50', '004');
 
-INSERT INTO expozice (id_expozice, typ, umelec, od, do, id_zamestnance) VALUES('001', 'socha≈ôsk√° instalace', 'Pavel Nov√°k', TO_DATE('2016-01-14','YYYY-MM-DD'), TO_DATE('2016-05-15','YYYY-MM-DD'), '002');
-INSERT INTO expozice (id_expozice, typ, umelec, od, do, id_zamestnance) VALUES('002', 'fotografick√° v√Ωstava', 'Anton√≠n Kratochv√≠l', TO_DATE('2016-03-12','YYYY-MM-DD'), TO_DATE('2016-12-04','YYYY-MM-DD'), '002');
-INSERT INTO expozice (id_expozice, typ, umelec, od, do, id_zamestnance) VALUES('003', 'mal√≠≈ôsk√° v√Ωstava', 'Fratni≈°ek Ringo ƒåech', TO_DATE('2016-12-04','YYYY-MM-DD'), TO_DATE('2016-12-05','YYYY-MM-DD'), '003');
+INSERT INTO umelec (id_umelce, jmeno, prijmeni, specializace, id_zamestnance) VALUES(null, 'Pavel', 'Nov·k', 'socha¯stvÌ', '001');
+INSERT INTO umelec (id_umelce, jmeno, prijmeni, specializace, id_zamestnance) VALUES(null, 'AntotnÌn', 'KratochvÌl', 'fotografie', '003');
+INSERT INTO umelec (id_umelce, jmeno, prijmeni, specializace, id_zamestnance) VALUES(null, 'Fratniöek Ringo', '»ech', 'malba', '003');
 
-INSERT INTO objednavka (id_objednavky, od, do, poplatek, id_pronajimatele, id_expozice, id_zamestnance) VALUES('001', TO_DATE('2016-04-12','YYYY-MM-DD'), TO_DATE('2016-05-12','YYYY-MM-DD'), 'ano', '003', '002', '001');
-INSERT INTO objednavka (id_objednavky, od, do, poplatek, id_pronajimatele, id_expozice, id_zamestnance) VALUES('002', TO_DATE('2016-04-01','YYYY-MM-DD'), TO_DATE('2016-05-05','YYYY-MM-DD'), 'ne', '001', '003', '001');
-INSERT INTO objednavka (id_objednavky, od, do, poplatek, id_pronajimatele, id_expozice, id_zamestnance) VALUES('003', TO_DATE('2016-03-12','YYYY-MM-DD'), TO_DATE('2016-04-12','YYYY-MM-DD'), 'ano', '002', '001', '002');
+INSERT INTO expozice (id_expozice, typ, umelec, od, do, id_zamestnance) VALUES(null, 'socha¯sk· instalace', 'Pavel Nov·k', TO_DATE('2016-01-14','YYYY-MM-DD'), TO_DATE('2016-05-15','YYYY-MM-DD'), '002');
+INSERT INTO expozice (id_expozice, typ, umelec, od, do, id_zamestnance) VALUES(null, 'fotografick· v˝stava', 'AntonÌn KratochvÌl', TO_DATE('2016-03-12','YYYY-MM-DD'), TO_DATE('2016-12-04','YYYY-MM-DD'), '002');
+INSERT INTO expozice (id_expozice, typ, umelec, od, do, id_zamestnance) VALUES(null, 'malÌ¯sk· v˝stava', 'Fratniöek Ringo »ech', TO_DATE('2016-12-04','YYYY-MM-DD'), TO_DATE('2016-12-05','YYYY-MM-DD'), '003');
+
+INSERT INTO objednavka (id_objednavky, od, do, poplatek, id_pronajimatele, id_expozice, id_zamestnance) VALUES(null, TO_DATE('2016-04-12','YYYY-MM-DD'), TO_DATE('2016-05-12','YYYY-MM-DD'), 'ano', '003', '002', '001');
+INSERT INTO objednavka (id_objednavky, od, do, poplatek, id_pronajimatele, id_expozice, id_zamestnance) VALUES(null, TO_DATE('2016-04-01','YYYY-MM-DD'), TO_DATE('2016-05-05','YYYY-MM-DD'), 'ne', '001', '003', '001');
+INSERT INTO objednavka (id_objednavky, od, do, poplatek, id_pronajimatele, id_expozice, id_zamestnance) VALUES(null, TO_DATE('2016-03-12','YYYY-MM-DD'), TO_DATE('2016-04-12','YYYY-MM-DD'), 'ano', '002', '001', '002');
 
 INSERT INTO mistnosti_objednavky(id_mistnosti, id_objednavky) VALUES ('001','001');
 INSERT INTO mistnosti_objednavky(id_mistnosti, id_objednavky) VALUES ('001','002');
@@ -176,42 +351,3 @@ INSERT INTO expozice_umelce(id_umelce, id_expozice) VALUES ('002','002');
 INSERT INTO expozice_umelce(id_umelce, id_expozice) VALUES ('003','003');
 
 COMMIT;
-
---kteri zamestnanec spravuje umelce s prijmenim Nov√°k
-SELECT Z.jmeno, Z.prijmeni
-FROM zamestnanec Z, umelec U
-WHERE U.prijmeni = 'Nov√°k' and U.id_zamestnance=Z.id_zamestnance;
-
---jake vybaveni je v mistnosti c.3
-SELECT V.typ, V.pocet
-FROM vybaveni_mistnosti V, mistnost M
-WHERE V.id_mistnosti=M.id_mistnosti and M.id_mistnosti='3';
-
---od kdy do kdy je objednana mistnost c.1
-SELECT O.od, O.do
-FROM objednavka O, mistnost M, mistnosti_objednavky MO
-WHERE M.id_mistnosti=MO.id_mistnosti and MO.id_objednavky=O.id_objednavky and M.id_mistnosti='001';
-
---kolik je vybaveni v jednotlivych mistnostech
-SELECT id_mistnosti, SUM(pocet) pocet_vybaveni
-FROM vybaveni_mistnosti
-GROUP BY id_mistnosti;
-
---za kterou mistnost se v objednavce plati nejvice
-SELECT MO.id_objednavky, MAX(M.cena)
-FROM mistnosti_objednavky MO, mistnost M
-WHERE MO.id_mistnosti=M.id_mistnosti
-GROUP BY MO.id_objednavky;
-
---kteri pronajimatele maji pouze jednu objednavku
-SELECT P.*
-FROM pronajimatel P, objednavka O
-WHERE O.id_pronajimatele=P.id_pronajimatele and NOT EXISTS
-    (SELECT *
-     FROM objednavka O1
-     WHERE O1.id_pronajimatele=P.id_pronajimatele and O.id_objednavky <> O1.id_objednavky);
-
---kteri zamestnanci nespravuji ani jednu mistnost
-SELECT *
-FROM zamestnanec
-WHERE id_zamestnance NOT IN ( SELECT id_zamestnance from mistnost);
